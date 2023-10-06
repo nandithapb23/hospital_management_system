9b8a5dcfb773d7396d249f41f4294e382a05c80f
nandithapb23 <122531318+nandithapb23@users.noreply.github.com>
2023-10-06 16:58:07 +0530

    Delete springboot_hospital_app directory

--- springboot_hospital_app/src/main/java/com/ty/springboot_hospital_app/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ty.springboot_hospital_app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.springboot_hospital_app.dto.Person;
-
-public interface PersonRepo extends JpaRepository<Person, Integer> {
-
-}
